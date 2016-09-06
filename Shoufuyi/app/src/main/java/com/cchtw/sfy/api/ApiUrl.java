99b7d20ac20a86140cbd3b00efa7c189119cbe8c
FuHL <814073395@qq.com>
2016-09-06 17:08:49 +0800

    添加新的环境

--- Shoufuyi/app/src/main/java/com/cchtw/sfy/api/ApiUrl.java
+++ Shoufuyi/app/src/main/java/com/cchtw/sfy/api/ApiUrl.java
@@ -4,8 +4,12 @@ public class ApiUrl {
 
     //开发环境
     public static final String DEV_BASE_URL = "";
-    //生产环境美服IP
-    public static final String ONLINE_URL = "http://www.itisfs.com/SfyRelayServices_v1/appRevServer";
+    //开发环境
+//    public static final String ONLINE_URL = "http://www.itisfs.com/SfyRelayServices_v1/appRevServer";
+    //正式环境
+//    public static final String ONLINE_URL = "http://cps.gnete.com/appRevServer";
+    //测试环境
+    public static final String ONLINE_URL = "http://cps.gnete.com:65/appRevServer";
 
     public static String getAbsoluteUrl(String relativeUrl) {
         String url = ONLINE_URL + relativeUrl;
