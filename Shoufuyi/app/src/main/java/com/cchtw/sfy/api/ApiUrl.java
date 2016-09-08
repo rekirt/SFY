155785b7ac2067bc50198b9a568def73ef687a68
FuHL <814073395@qq.com>
2016-09-08 16:26:58 +0800

    更新控件密钥，重新打包

--- Shoufuyi/app/src/main/java/com/cchtw/sfy/api/ApiUrl.java
+++ Shoufuyi/app/src/main/java/com/cchtw/sfy/api/ApiUrl.java
@@ -7,9 +7,9 @@ public class ApiUrl {
     //开发环境
 //    public static final String ONLINE_URL = "http://www.itisfs.com/SfyRelayServices_v1/appRevServer";
     //正式环境
-//    public static final String ONLINE_URL = "http://cps.gnete.com/appRevServer";
+    public static final String ONLINE_URL = "http://cps.gnete.com/appRevServer";
     //测试环境
-    public static final String ONLINE_URL = "http://cps.gnete.com:65/appRevServer";
+//    public static final String ONLINE_URL = "http://cps.gnete.com:65/appRevServer";
 
     public static String getAbsoluteUrl(String relativeUrl) {
         String url = ONLINE_URL + relativeUrl;
