b6d3f6ea8a61c7abe35ccb4d4ef5cd4ccd14ab21
FuHL <814073395@qq.com>
2016-09-09 17:30:08 +0800

    修改了压缩图片的方法

--- Shoufuyi/app/src/main/java/com/cchtw/sfy/uitls/cache/ACache.java
+++ Shoufuyi/app/src/main/java/com/cchtw/sfy/uitls/cache/ACache.java
@@ -41,7 +41,7 @@ public class ACache {
 	public static final int TIME_DAY = TIME_HOUR * 24;
 	public static final int TIME_MONTH = TIME_HOUR * 24*30;
 	public static int TIME_CACHE = TIME_MONTH;
-	private static final int MAX_SIZE = 1000 * 1000 * 50; // 50 mb
+	private static final int MAX_SIZE = 1000 * 1000 * 100; // 50 mb
 	private static final int MAX_COUNT = Integer.MAX_VALUE; // 不限制存放数据的数量
 	private static Map<String, ACache> mInstanceMap = new HashMap<String, ACache>();
 	private ACacheManager mCache;
